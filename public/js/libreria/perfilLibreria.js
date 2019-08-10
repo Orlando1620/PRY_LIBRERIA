d39735dde816fddc53940638112337d4c43a9ddb
Pablo <ocampov98@gmail.com>
2019-08-10 13:12:06 -0600

    Eliminar Lib

--- public/js/libreria/perfilLibreria.js
+++ public/js/libreria/perfilLibreria.js
@@ -42,4 +42,52 @@ function perfilLibreria(pnombreLibreria){
     );
 }
 
+async function delLib(){
+  try{
+    var sucursal;
+    var id;
+  
+    for(var i=0;i<sucursales.length;i++){
+      sucursal = sucursales[i]["nombreSucursal"];
+      id = sucursales[i]["_id"];
+      
+      var dataInv = {
+        nombreSuc:sucursal
+      }
+      var inv = await fetch('/inventario/eliminarTodo', {
+          method: 'POST',
+          body: JSON.stringify(dataInv),
+          headers:{'Content-Type': 'application/json'}
+      });
+  
+      var dataSuc = {
+          id:id
+      }
+      var suc = await fetch('/sucursal/eliminar', {
+          method: 'POST',
+          body: JSON.stringify(dataSuc),
+          headers:{'Content-Type': 'application/json'}
+      });
+    }  
+  
+
+    var data = {
+        libreria: sessionStorage.getItem("nombreLibreria")
+    }
+
+    await fetch('/libreria/eliminar', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{'Content-Type': 'application/json'}
+    });
+
+    window.location.href = "listar-librerias-adminGlobal.html";
+
+
+  } catch(err){
+      console.log('Ocurrió un error con la ejecución', err);
+  }
+}
+
+
 perfilLibreria(sessionStorage.getItem("nombreLibreria"));
