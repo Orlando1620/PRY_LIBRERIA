1e8e5becbf10af94a01c476a6a6d31385bde0c27
Pablo <ocampov98@gmail.com>
2019-08-12 12:57:40 -0600

    Reg Promocion

--- api/promocion/promocion.model.js
+++ api/promocion/promocion.model.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 
 var sucursalSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  libroPromo: String,
+  sucursal: String,
+  libro: String,
   porcentaje: Number,
   fechaInicio: Date,
-  fechaFinaliza: Date
+  fechaFinaliza: Date,
+  imgUrl: String
 });
 
 module.exports = mongoose.model('Promocion', sucursalSchema,'Promociones');
--- api/promocion/promocion.route.js
+++ api/promocion/promocion.route.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var sucursalApi = require('./promocion.api');
+var promocionApi = require('./promocion.api');
 
 
 router.post('/add', function(req, res) {
-  sucursalApi.registrarPromocion(req,res);
+  promocionApi.registrarPromocion(req,res);
+})
+
+router.post('/localUploadImg', function (req, res){
+  promocionApi.localUploadImg(req,res);
 })
 
 module.exports = router;
