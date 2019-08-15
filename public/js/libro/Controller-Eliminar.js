9d03de7a5b2eeb76f2bf571086e8c70b16cde422
Orlando <otrejosm@ucenfotec.ac.cr>
2019-08-15 12:03:18 -0600

    Partes de eliminar libro

--- public/js/libro/Controller-Eliminar.js
+++ public/js/libro/Controller-Eliminar.js
@@ -1,3 +1,28 @@
-async function eliminarLibro(){
-    
+var tmp;
+
+async function eliminarLibro() {
+    var idLibro = sessionStorage.getItem('idLibro');
+    var nombrelibro = document.getElementById("titulo").innerHTML;
+
+    var tmp = await obtenerDatoAsociacionLibro(nombrelibro, idLibro);
+
+    if (tmp) {
+        //mostrarle el msj que no lo puede eliminar
+
+    } else {
+        //mostrarle el msj si de verdad lo quiere eliminar
+        //cuando le de que si 
+    }
+
+}
+
+
+function aceptar() {
+    document.getElementById("pop-up").classList.add("oculto");
+    //eliminarLibro();
+}
+
+
+function cancelar() {
+    document.getElementById("pop-up").classList.add("oculto");
 }
